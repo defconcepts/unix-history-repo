defconcepts/unix-history-repo

commit 311f88e0ffacdab0ad1d0011a44959a949211c7c

Plumb support for the device advanced information CCB in the ATA XPT.
This was previously done only for SCSI XPT in r223081, on which the change
in r223089 depended in order to respond to serial number requests.  As a
result of r223089, da(4) and ada(4) devices register a d_getattr for geom to
use to obtain the information.

Reported by:	ache
Reviewed by:	ken


git-svn-id: svn+ssh://svn.freebsd.org/base/head@223443 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/cam/ata/ata_xpt.c
@@ -1601,6 +1601,34 @@ ata_device_transport(struct cam_path *path)
 }
 
 static void
+ata_dev_advinfo(union ccb *start_ccb)
+{
+	struct cam_ed *device;
+	struct ccb_dev_advinfo *cdai;
+	off_t amt; 
+
+	start_ccb->ccb_h.status = CAM_REQ_INVALID;
+	device = start_ccb->ccb_h.path->device;
+	cdai = &start_ccb->cdai;
+	switch(cdai->buftype) {
+	case CDAI_TYPE_SERIAL_NUM:
+		if (cdai->flags & CDAI_FLAG_STORE)
+			break;
+		start_ccb->ccb_h.status = CAM_REQ_CMP;
+		cdai->provsiz = device->serial_num_len;
+		if (device->serial_num_len == 0)
+			break;
+		amt = device->serial_num_len;
+		if (cdai->provsiz > cdai->bufsiz)
+			amt = cdai->bufsiz;
+		memcpy(cdai->buf, device->serial_num, amt);
+		break;
+	default:
+		break;
+	}
+}
+
+static void
 ata_action(union ccb *start_ccb)
 {
 
@@ -1651,6 +1679,11 @@ ata_action(union ccb *start_ccb)
 			break;
 		}
 		/* FALLTHROUGH */
+	}
+	case XPT_DEV_ADVINFO:
+	{
+		ata_dev_advinfo(start_ccb);
+		break;
 	}
 	default:
 		xpt_action_default(start_ccb);

commit 027a8c71e55ef6cdefb8cfb4c5ee2e1abb21eefd

Fix ATAPI breakage introduced by r223443. It made SCSI commands to ATAPI device to never complete, that caused probe process (system boot) stuck.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@223475 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/cam/ata/ata_xpt.c
@@ -1678,7 +1678,8 @@ ata_action(union ccb *start_ccb)
 			xpt_done(start_ccb);
 			break;
 		}
-		/* FALLTHROUGH */
+		xpt_action_default(start_ccb);
+		break;
 	}
 	case XPT_DEV_ADVINFO:
 	{